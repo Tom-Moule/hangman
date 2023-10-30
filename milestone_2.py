fd80f2bc7058bb7eef0744038eef9383cd61f878
tom moule <tjmoule1991@gmail.com>
2023-10-30 10:35:18 +0000

    Creates word_list variable, and selects word at random. Asks user to input a letter and validates

---
+++ milestone_2.py
@@ -0,0 +1,23 @@
+word_list = ["apple", "banana", "raspberry", "grape", "mango"]
+
+print(word_list)
+
+import random
+
+word = random.choice(word_list)
+
+print(word)
+
+guess = input("select a single letter")
+
+if len(guess) == 1 and guess.isalpha():
+
+  print ("Good Guess")
+
+else:
+
+  print("Oops! That is not a valid input.")
+
+
+
+
